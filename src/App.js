1de05d9becc66e216e10669a3417ef5eb357ea2c
COTIC <coticproeg@gmail.com>
2020-01-30 11:52:08 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './components/Nav/Index';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Nav = () => (
+    <nav className="navbar navbar-toggleable-md fixec-top">
+        <div className="collapse navbar-collapse container">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link">
+                        <i class="oction oction-home"></i>
+                            Home
+                    </a>
+                </li>
+
+                <li className="nav-item active">
+                    <a className="nav-link">
+                        <i class="oction oction-home"></i>
+                            Moments
+                        </a>
+                </li>
+                <li className="nav-item active">
+                    <a className="nav-link">
+                        <i class="oction oction-home"></i>
+                            Notifications
+                    </a>
+                </li>
+                <li className="nav-item active">
+                    <a className="nav-link">
+                        <i class="oction oction-home"></i>
+                            Messages
+                    </a>
+                </li>
+            </ul>
+            <form></form>
+            <div>
+            </div>
+            <button>
+            </button>
+        </div>
+    </nav>
+)
+
+export default Nav;
